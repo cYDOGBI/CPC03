bb8ae8f0498608580f96a14a4d7512fa6ee630bc
Tiago Pinho <60488719+cYDOGBI@users.noreply.github.com>
2025-07-25 13:43:01 +0100

    Add files via upload

---
+++ ex00/ft_strcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaranje <tlaranje@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 14:20:37 by tlaranje          #+#    #+#             */
+/*   Updated: 2025/07/25 09:48:16 by tlaranje         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+/*
+int	main(void)
+{
+	printf("%d\n", ft_strcmp("Hello", "Hello"));
+	printf("%d\n", ft_strcmp("zfz", "gfg"));
+	printf("%d\n", ft_strcmp("bgb", "gfg"));
+	return (0);
+}*/
---
+++ ex01/ft_strncmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaranje <tlaranje@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 14:20:37 by tlaranje          #+#    #+#             */
+/*   Updated: 2025/07/25 09:48:16 by tlaranje         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n - 1 && s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+/*
+int	main(void)
+{
+	printf("%d\n", ft_strncmp("abcd", "abcd", 1));
+	printf("%d\n", ft_strncmp("abcd", "atcd", 2));
+	printf("%d\n", ft_strncmp("abtd", "abcd", 3));
+	return (0);
+}*/
